1b138b901c18cf9863e6538c3069d8aedee227a9
FaridehBahari <bahari.faride@gmail.com>
2024-10-13 13:52:12 +0330

    local mutation rate

--- baseline/local_mutRates.R
+++ baseline/local_mutRates.R
@@ -1,11 +1,73 @@
-######################## strategy number 1 ###################################
-# just extend each block to 100kb window
 rm(list = ls())
-# setwd('C:/Active/projects/make_features/BMR/')
+
 library(GenomicRanges)
 library(rtracklayer)
 library(dplyr)
 library(data.table)
+
+path_bed <- '../external/database/bins/proccessed/PCAWG_callable.bed6'
+win_size <- 100000
+
+
+gr_elements <- import.bed(path_bed)
+gr_elements$blockID <- paste0('b', 1:length(gr_elements))
+
+# gr_extended <- resize(gr_elements, width = width(gr_elements) + 100000, fix = "center")
+# gr_split <- split(gr_extended, gr_extended$name)
+gr_split <- resize(gr_elements, width = width(gr_elements) + win_size, fix = "center")
+gr_split <- split(gr_split, gr_split$name)
+
+
+library(doParallel)
+library(foreach)
+
+
+num_cores <- 40  
+registerDoParallel(num_cores)
+
+t0 = Sys.time()
+# Step 3: Parallelize the loop using foreach
+filtered_bg <- foreach(i = 1:length(gr_split), .combine = c, .packages = "GenomicRanges") %dopar% {
+  if (i %% 5000 == 0) {
+    print(i) # i = 121790
+  }
+  
+  gr_elem <- gr_elements[which(gr_elements$name == names(gr_split)[i])]
+  bg_containElem <- gr_split[[i]]
+  gr_elem$length <- width(gr_elem)
+  
+  filtered_bg_grs <- GRanges()
+  # Remove overlaps within each element
+  for (j in 1:length(bg_containElem)) {
+    filtered_bg_gr <- GenomicRanges::setdiff(bg_containElem[j], gr_elem)
+    filtered_bg_gr$name <- unique(gr_elem$name)
+    ID = bg_containElem[j]$blockID
+    filtered_bg_gr$blockID <- ID
+    filtered_bg_gr$length = gr_elem[which(gr_elem$blockID == ID)]$length
+    filtered_bg_grs <- c(filtered_bg_grs, filtered_bg_gr)
+  }
+  
+  return(filtered_bg_grs)
+}
+
+
+print(filtered_bg)
+
+print(Sys.time() - t0)
+
+# filtered_bg <- GRanges()
+# for (i in 1:length(gr_split)) {
+#   if (i%%5000 == 0) {
+#     print(i)
+#   }
+#   gr_elem <- gr_elements[which(gr_elements$name == names(gr_split)[i])]
+#   
+#   # Remove overlaps within each element
+#   filtered_bg_gr = GenomicRanges::setdiff(gr_split[[i]], gr_elem)
+#   filtered_bg_gr$name <- unique(gr_elem$name)
+#   filtered_bg <- c(filtered_bg, filtered_bg_gr)
+# }
+
 
 
 select_cohort <- function(path_donorInfo, 
@@ -38,57 +100,6 @@ select_cohort <- function(path_donorInfo,
   
 }
 
-create_downstream_background = function(gr_elements, win_size, this_genome) {
-  
-  
-  # background is plus/minus window around every component of element
-  # make sure flanking coordinates stay within chromosomal boundaries
-  # add one extra nucleotide of slack on both ends - trinuc tabulation needs that
-  
-  bg_starts = GenomicRanges::start(gr_elements) - win_size
-  bg_ends = GenomicRanges::start(gr_elements) - 1
-  
-  # max pos is end of chromosome minus one
-  max_chr_pos = GenomeInfoDb::seqlengths(this_genome)[as.character(GenomicRanges::seqnames(gr_elements))]
-  max_chr_pos = max_chr_pos - 1
-  
-  # min pos is 2nd pos of chromosome
-  bg_starts[bg_starts < 2] = 2
-  bg_ends[bg_ends > max_chr_pos] = max_chr_pos[bg_ends > max_chr_pos]
-  gr_background = GenomicRanges::GRanges(GenomicRanges::seqnames(gr_elements), 
-                                         IRanges::IRanges(bg_starts, bg_ends))
-  
-  mcols(gr_background) <- gr_elements$name
-  names(mcols(gr_background)) <- gsub("^X$", "name", names(mcols(gr_background)))
-  gr_background
-}
-
-
-create_upstream_background = function(gr_elements, win_size, this_genome) {
-  
-  
-  # background is plus/minus window around every component of element
-  # make sure flanking coordinates stay within chromosomal boundaries
-  # add one extra nucleotide of slack on both ends - trinuc tabulation needs that
-  
-  bg_starts = GenomicRanges::end(gr_elements) + 1 
-  bg_ends = GenomicRanges::end(gr_elements) + win_size 
-  
-  # max pos is end of chromosome minus one
-  max_chr_pos = GenomeInfoDb::seqlengths(this_genome)[as.character(GenomicRanges::seqnames(gr_elements))]
-  max_chr_pos = max_chr_pos - 1
-  
-  # min pos is 2nd pos of chromosome
-  bg_starts[bg_starts < 2] = 2
-  bg_ends[bg_ends > max_chr_pos] = max_chr_pos[bg_ends > max_chr_pos]
-  gr_background = GenomicRanges::GRanges(GenomicRanges::seqnames(gr_elements), 
-                                         IRanges::IRanges(bg_starts, bg_ends))
-  
-  mcols(gr_background) <- gr_elements$name
-  names(mcols(gr_background)) <- gsub("^X$", "name", names(mcols(gr_background)))
-  gr_background
-  
-}
 
 map_muts <- function(gr, testGE_gr){
   
@@ -136,6 +147,7 @@ map_muts <- function(gr, testGE_gr){
 }
 
 
+
 create_response <- function(all_elements, n_donors){
   
   obs_Muts <- all_elements$MAF_GE_mapped
@@ -170,41 +182,26 @@ create_response <- function(all_elements, n_donors){
   return(resTab)
 }
 
-#################### prepare input ###########
-path_bed <- '../external/database/bins/proccessed/PCAWG_callable.bed6'
-win_size <- 50000
-this_genome <- BSgenome.Hsapiens.UCSC.hg19::Hsapiens
 
-gr_elements <- import.bed(path_bed)
-gr_elements$PCAWG_ID <- gr_elements$name
-gr_elements$name <- paste0('ele', 1:length(gr_elements) )
-
-
-
-#### create background bed ####
-gr_background_ds = create_downstream_background(gr_elements, win_size, this_genome)
-gr_background_us = create_upstream_background(gr_elements, win_size, this_genome)
-
-bg_bed <- c(gr_background_ds, gr_background_us)
-######### annotate muts ##########
-dir.create('../external/BMR/output/Res_reviewerComments/baseLine_localRates/input/', recursive=T)
+############################### create block-wise background response tables ####################
+dir.create('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/', recursive=T)
 load('../../iDriver/extdata/procInput/mut_PCAWG/all_samples.RData')
+gr <- gr[!duplicated(gr)]
 
 path_donorInfo <- '../../iDriver/extdata/procInput/iDriverInputs/donorInfo.tsv'
 
-# cohort <- 'Pancan-no-skin-melanoma-lymph'
 
 cohorts <- c('Pancan-no-skin-melanoma-lymph', "Liver-HCC" ,
-              "Bladder-TCC", "ColoRect-AdenoCA", "Lymph-BNHL",
-              "Uterus-AdenoCA", "Kidney-RCC", "Lymph-CLL", "Lung-SCC",
-              "Stomach-AdenoCA", "Skin-Melanoma", "Panc-Endocrine", "Head-SCC",
-              "Breast-AdenoCa","Biliary-AdenoCA", "Eso-AdenoCa", "CNS-GBM",
-              "Panc-AdenoCA", "Lung-AdenoCA", "Prost-AdenoCA", "Ovary-AdenoCA",
-              "Thy-AdenoCA", "Myeloid-MPN", "Bone-Leiomyo",
-              "CNS-Medullo", "CNS-Oligo", "Cervix-SCC",
-              "CNS-PiloAstro", "Kidney-ChRCC", "Bone-Osteosarc",
-              "Breast-LobularCa",  "Lymph-NOS", "Myeloid-AML", "Bone-Epith",
-              "Cervix-AdenoCA","Breast-DCIS","Bone-Cart" #,"Myeloid-MDS"
+  "Bladder-TCC", "ColoRect-AdenoCA", "Lymph-BNHL",
+  "Uterus-AdenoCA", "Kidney-RCC", "Lymph-CLL", "Lung-SCC",
+  "Stomach-AdenoCA", "Skin-Melanoma", "Panc-Endocrine", "Head-SCC",
+  "Breast-AdenoCa","Biliary-AdenoCA", "Eso-AdenoCa", "CNS-GBM",
+  "Panc-AdenoCA", "Lung-AdenoCA", "Prost-AdenoCA", "Ovary-AdenoCA",
+  "Thy-AdenoCA", "Myeloid-MPN", "Bone-Leiomyo",
+  "CNS-Medullo", "CNS-Oligo", "Cervix-SCC",
+  "CNS-PiloAstro", "Kidney-ChRCC", "Bone-Osteosarc",
+  "Breast-LobularCa",  "Lymph-NOS", "Myeloid-AML", "Bone-Epith",
+  "Cervix-AdenoCA","Breast-DCIS","Bone-Cart" #,"Myeloid-MDS"
 ) 
 
 
@@ -213,57 +210,107 @@ for (cohort in cohorts) {
   print(cohort)
   
   exclude_lymph_melanoma <- ifelse(cohort == 'Pancan-no-skin-melanoma-lymph', T, F)
+  
+  
+  
+  
   donorInfo <- select_cohort(path_donorInfo,
                              cohort,
                              exclude_lymph_melanoma,
                              exclude_hyper_mutated = T)
   
   gr_cohort <- gr[which(gr$D_id %in% donorInfo$donor_id)]
+  
+  
+  
+  bg_bed <- filtered_bg
+  bg_bed$PCAWG_ID <- bg_bed$name
+  bg_bed$name <- bg_bed$blockID
+  
+  t0 <- Sys.time()
   all_elements <- map_muts(gr_cohort, bg_bed)
+  print(paste0('map_muts total time: ', Sys.time() - t0))
   
   n_donors <- length(unique(gr_cohort$D_id))
+  
+  t0 <- Sys.time()
   resTab <- create_response(all_elements, n_donors)
+  print(paste0('create_response total time: ', Sys.time() - t0))
   
-  ele_PCAWGs = data.frame('background' = gr_elements$name,
-                          'PCAWG_ID' = gr_elements$PCAWG_ID, 
-                          'length_PCAWG_ID' = width(gr_elements))
+  resTab
   
-  final_resTab = left_join(resTab, ele_PCAWGs, by = c('binID' = 'background'))
-  colnames(final_resTab)[1] = 'background_id'
-  fwrite(final_resTab, file = paste0('../external/BMR/output/Res_reviewerComments/baseLine_localRates/input/',
-                                     cohort, '_responseTable_all100k.tsv'), sep = '\t')
+  df <- data.frame('PCAWG_ID' = bg_bed$PCAWG_ID,
+                   'elem_ID' = bg_bed$name, 
+                   'PCAWG_block_length' = bg_bed$length)
   
-  ##### assesments bg1 ####
+  df <- df[!duplicated(df),]
+  
+  pred_tab <- left_join(resTab, df, by = c('binID'= 'elem_ID'))
+  pred_tab <- pred_tab[!duplicated(pred_tab),]
+  # pred_tab <- as.data.frame(left_join(resTab, df, by = c('binID'= 'elem_ID')))
+  # pred_tab <- pred_tab[,which(!colnames(pred_tab) %in% c('binID', 'PCAWG_block_length'))]
+  # pred_tab <- pred_tab[!duplicated(pred_tab),]
+  
+  
+  fwrite(pred_tab, file = paste0('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/',cohort, '.tsv'), sep = '\t')
+}
+
+################################# estimate mutation rate based on weighted means of block lengths ###############################################
+rm(list = ls())
+library(data.table)
+library(dplyr)
+
+# setwd('C:/Active/projects/make_features/BMR/')
+
+dir.create('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/assessments/', showWarnings = F, recursive = T)
+dir.create('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/predRates/', showWarnings = F, recursive = T)
+ann <- fread('../external/BMR/procInput/ann_PCAWG_ID_complement.csv', sep = ',')
+
+
+ann <- ann[(ann$in_CGC == TRUE) | (ann$in_CGC_literature == TRUE) | 
+             (ann$in_CGC_new == TRUE) | (ann$in_oncoKB == TRUE) | 
+             (ann$in_pcawg == TRUE), ]
+drivers <- ann$PCAWG_IDs
+cohorts <- c('Pancan-no-skin-melanoma-lymph', "Liver-HCC" ,
+             "Bladder-TCC", "ColoRect-AdenoCA", "Lymph-BNHL",
+             "Uterus-AdenoCA", "Kidney-RCC", "Lymph-CLL", "Lung-SCC",
+             "Stomach-AdenoCA", "Skin-Melanoma", "Panc-Endocrine", "Head-SCC",
+             "Breast-AdenoCa","Biliary-AdenoCA", "Eso-AdenoCa", "CNS-GBM",
+             "Panc-AdenoCA", "Lung-AdenoCA", "Prost-AdenoCA", "Ovary-AdenoCA",
+             "Thy-AdenoCA", "Myeloid-MPN", "Bone-Leiomyo",
+             "CNS-Medullo", "CNS-Oligo", "Cervix-SCC",
+             "CNS-PiloAstro", "Kidney-ChRCC", "Bone-Osteosarc",
+             "Breast-LobularCa",  "Lymph-NOS", "Myeloid-AML", "Bone-Epith",
+             "Cervix-AdenoCA","Breast-DCIS","Bone-Cart" #,"Myeloid-MDS"
+) 
+
+assessments <- c()
+for (cohort in cohorts) {
+  
+  print(cohort)
+  
   test_y <- fread(paste0('../../iDriver/extdata/procInput/BMRs/observed/', cohort,
                          '/test_y.tsv'))
   test_y$obsRate <- test_y$nMut/test_y$length
   
-  # obs <- test_y[which(test_y$binID == 'gc19_pc.3utr::gencode::AL627309.1::ENSG00000237683.5'),]
-  # x = df[which(df$PCAWG_ID == 'gc19_pc.3utr::gencode::AL627309.1::ENSG00000237683.5'),]
-  # 
-  # total_length = sum(x$length_PCAWG_ID)
-  # weights <- x$length_PCAWG_ID/total_length
-  # 
-  # rates <- x$nMut/x$length
-  # pred_elem <- sum(weights*rates)/length(rates)
-  # obs_elem = obs$nMut/obs$length
-  
+  final_resTab <- fread(paste0('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/',cohort, '.tsv'))
   
   obs_tab <- test_y[,c('binID', 'obsRate')]
   df <- left_join(final_resTab, obs_tab, by = c('PCAWG_ID' = 'binID'))
   df$nMut = as.numeric( df$nMut)
   df$length = as.numeric( df$length)
-  df$length_PCAWG_ID = as.numeric(df$length_PCAWG_ID)
+  df$PCAWG_block_length = as.numeric(df$PCAWG_block_length)
+  
   
   # Efficient calculation using dplyr
   results <- df %>%
     group_by(PCAWG_ID) %>%
     summarise(
-      total_length = sum(length_PCAWG_ID),
+      total_length = sum(PCAWG_block_length),
       rates = nMut / length,
-      pred_elem = sum((length_PCAWG_ID / sum(length_PCAWG_ID)) * rates) / n(),
-      obs_elem = unique(obsRate)
-    ) %>%
+      pred_elem = sum((PCAWG_block_length / sum(PCAWG_block_length)) * rates) / n(),
+      obs_elem = unique(obsRate),
+      .groups = "drop") %>%
     ungroup()
   
   results2 <- results[,c('PCAWG_ID', 'total_length', 'pred_elem', 'obs_elem')]
@@ -279,9 +326,13 @@ for (cohort in cohorts) {
   }
   
   results2$elemType <- define_element_type(results2$PCAWG_ID)
+  fwrite(results2, paste0('../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/predRates/',cohort, '.tsv'), sep = '\t')
   
   elems <- unique(results2$elemType)
   
+  # remove known drivers from assessments
+  results2 <- results2[!(results2$PCAWG_ID %in% drivers), ]
+  results2 <- as.data.frame(results2)
   corrs <- c()
   for (elemType in elems) {
     elem <- results2[which(results2$elemType == elemType),]
@@ -289,8 +340,13 @@ for (cohort in cohorts) {
   }
   
   ass <- data.frame('baseline_corr' = corrs, 'element-type' = elems)
-  fwrite(ass, file = paste0('../external/BMR/output/Res_reviewerComments/baseLine_localRates/',
-                            cohort, '_baselineAssessment.tsv'), sep = '\t')
+  ass$cohort <- cohort
+  assessments <- rbind(assessments, ass)
   
 }
+
+
+fwrite(assessments, file = '../external/BMR/output/Res_reviewerComments/baseLine_localRates/final/assessments/all_cohorts_assessments.tsv',
+       , sep = '\t')
+
 
