255b03f732bbacd483b15cff594cea414dc562e2
FaridehBahari <bahari.faride@gmail.com>
2024-10-27 16:17:15 +0330

    wip

--- inference/infer.py
+++ inference/infer.py
@@ -23,16 +23,9 @@ from readFtrs_Rspns import read_response
 def read_pred(path_pred):
     Y_pred = pd.read_csv(path_pred, sep = "\t", header=0, index_col='binID',
                          usecols=['binID', 'predRate'])
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv('../external/BMR/procInput/ann_PCAWG_ID_complement.csv', sep=',')
-
-    filtered_df = df[(df['in_CGC'] | df['in_CGC_literature'] | df['in_CGC_new'] | df['in_oncoKB'] | df['in_pcawg'])]
-        
-    # Select the 'PCAWG_IDs' column from the filtered DataFrame
-    drivers = filtered_df['PCAWG_IDs']
-    Y = Y_pred.loc[~(Y_pred.index).isin(drivers)]
+   
     
-    return Y
+    return Y_pred
 
 
 def dispersion_test(yhat, y, k=100):
