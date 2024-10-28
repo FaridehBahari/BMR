c399b16592915a9975096f984b701701985f5a75
FaridehBahari <bahari.faride@gmail.com>
2024-10-28 14:04:59 +0330

    compute p-val models

--- models/repeated_train_test.py
+++ models/repeated_train_test.py
@@ -11,7 +11,7 @@ def get_metric_value(df, metric):
     index_values = [str(index) for index in df.index if not pd.isna(index)]
     
     metric_index = [index for index in index_values if metric_pattern.match(index)]
-
+    
     if metric_index:
         metric_value = df.loc[metric_index[0], 'metric_value']
         return metric_value
@@ -23,7 +23,7 @@ def get_metric_value(df, metric):
 def extract_metric_values_model(dir_path, metric):
     # Define a regular expression pattern to match files with '_M\d_assessment.tsv' format
     file_pattern = re.compile(r'_M(\d+)_assessment\.tsv')
-
+    
     # Get all files in the directory matching the pattern
     files = [f for f in os.listdir(dir_path) if file_pattern.search(f)]
     
@@ -31,11 +31,11 @@ def extract_metric_values_model(dir_path, metric):
     # Iterate through each file
     for file in files:
         file_path = os.path.join(dir_path, file)
-
+        
         # Read the TSV file into a DataFrame
         df = pd.read_csv(file_path, sep='\t', index_col=0, header=None, 
                          names=['metric_value'])
-
+        
         # Extract the metric value based on the provided metric argument
         metric_value = get_metric_value(df, metric)
         metric_values.append(metric_value)
@@ -50,10 +50,10 @@ def Exp_Var_performance_onValSets(dir_path, metric):
     
     # Convert the list to a NumPy array
     metric_values_array = np.array(metric_values)
-
+    
     # Convert the NumPy array to floating-point numbers
     metric_values_array = metric_values_array.astype(float)
-
+    
     # Calculate the mean and variance
     mean_value = metric_values_array.mean()
     var_value =  metric_values_array.var()
@@ -90,10 +90,15 @@ def compare_two_models(dir_path1, dir_path2, metric):
     M1_metrics = extract_metric_values_model(dir_path1, metric)
     M2_metrics = extract_metric_values_model(dir_path2, metric)
     
+    M1_metrics = list(map(float, M1_metrics)) 
+    M2_metrics = list(map(float, M2_metrics))
+    
     t_statistic, p_value = ttest_rel(M1_metrics, M2_metrics)
     
-    return t_statistic, p_value
-        
+    # return t_statistic, p_value
+    return p_value
+      
+
 #############################################################################
 # # EXAMPLE
 # # calculate the average and variance of repeated models performance on validation sets:
